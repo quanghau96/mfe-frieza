3a7fcaa558c85f2f7120c207e0d82fc776016f9a
Hau Vo <hau.vo@s3corp.com.vn>
2021-08-22 22:55:51 +0700

    config webpack for build production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,11 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
+const path = require('path');
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    path: path.join(__dirname, '..', 'build'),
   },
   plugins: [
     new ModuleFederationPlugin({
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -34,12 +34,11 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_frieza-app');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
-}
+
 
 export { mount };
